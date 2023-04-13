804a5cd5b33a8bcb16cc847b70bdbd0f7e7d4143
simonab101 <simo.buscicchio@gmail.com>
2023-04-13 15:29:24 +0200

    [add] image

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let vue = new Vue({
   el: "#myApp",
   data: {
       message: "Hello World!",
-      image: "" 
+      image: "https://www.maestralsolutions.com/m/u/2019/12/1_ekaFqk38CV2GNTquivy7Dw.png" 
   }
 })
